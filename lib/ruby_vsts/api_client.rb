1eea45f881f02670e84dad16a8cb39fa1172b7de
Gabor Lengyel <glengyel@prodexity.com>
2017-04-08 03:48:25 +0200

    changed methods in APIClient to class methods so models can easily call them

--- lib/ruby_vsts/api_client.rb
+++ lib/ruby_vsts/api_client.rb
@@ -7,23 +7,24 @@ module VSTS
   # API client for Visual Studio Team Services (VSTS)
   # Manages access tokens and API versions, builds proper requests as expected by the VSTS API
   class APIClient
-    # Constructor with dependency injection
-    #
-    # @param rest_client [RestClient] the RestClient module to be used
-    def initialize(rest_client = nil)
-      @rest_client = rest_client || RestClient
-    end
-
     # Make an API request
     #
     # @param method [Symbol] the method to be used, can be :get, :put, :post, :delete or :head (will be passed to RestClient)
     # @param resource [String] the resource to request under the base_url (ie. "/changesets")
-    # @param opts [Hash] options for the request, see #initialize for values, can have :payload and :urlparams in addition to that
+    # @param opts [Hash] options for the request
+    # @option opts [Hash] :payload payload for the request (if any)
+    # @option opts [String] :api_version
+    # @option opts [String] :collection
+    # @option opts [String] :team_project
+    # @option opts [String] :area
+    # @option opts [Hash] :urlparams
     # @return [Hash] request results as parsed from json
-    def request(method, resource, opts = {})
-      resp = @rest_client::Request.execute(
+    def self.request(method, resource, opts = {})
+      url = build_url(resource, opts)
+      VSTS.logger.debug("VSTS request: #{method} #{url}") if VSTS.configuration.debug
+      resp = RestClient::Request.execute(
         method: method,
-        url: build_url(resource, opts),
+        url: url,
         payload: opts[:payload],
         headers: {
           Authorization: authz_header_value,
@@ -39,7 +40,7 @@ module VSTS
     # @param resource [String] the resource to request under the base_url (ie. "/changesets")
     # @param opts [Hash] query options, see #request
     # @return [Hash] request results as parsed from json
-    def get(resource, opts = {})
+    def self.get(resource, opts = {})
       request(:get, resource, opts)
     end
 
@@ -49,7 +50,7 @@ module VSTS
     # @param payload [Hash] payload to be sent with the request, takes precedence over opts[:payload]
     # @param opts [Hash] query options, see #request
     # @return [Hash] request results as parsed from json
-    def post(resource, payload, opts = {})
+    def self.post(resource, payload, opts = {})
       opts[:payload] = payload
       request(:post, resource, opts)
     end
@@ -60,7 +61,7 @@ module VSTS
     # @param payload [Hash] payload to be sent with the request, takes precedence over opts[:payload]
     # @param opts [Hash] query options, see #request
     # @return [Hash] request results as parsed from json
-    def put(resource, payload, opts = {})
+    def self.put(resource, payload, opts = {})
       opts[:payload] = payload
       request(:put, resource, opts)
     end
@@ -71,7 +72,7 @@ module VSTS
     # @param payload [Hash] payload to be sent with the request, takes precedence over opts[:payload]
     # @param opts [Hash] query options, see #request
     # @return [Hash] request results as parsed from json
-    def patch(resource, payload, opts = {})
+    def self.patch(resource, payload, opts = {})
       opts[:payload] = payload
       request(:patch, resource, opts)
     end
@@ -81,18 +82,19 @@ module VSTS
     # @param resource [String] the resource to request under the base_url (ie. "/changesets")
     # @param opts [Hash] query options, see #request
     # @return [Hash] request results as parsed from json
-    def delete(resource, opts = {})
+    def self.delete(resource, opts = {})
       request(:delete, resource, opts)
     end
 
-    private
+    # Private class methods
 
     # Builds VSTS url as described in https://www.visualstudio.com/en-us/docs/integrate/get-started/rest/basics
     #
     # @param resource [String] the VSTS resource
-    # @param opts [Hash] options hash, see #initialize for values, can have :urlparams in addition to that
+    # @param opts [Hash] options hash, see #request
     # @return [String] the request URL
-    def build_url(resource, opts = {})
+    # @private
+    def self.build_url(resource, opts = {})
       base_url = VSTS.configuration.base_url.sub(%r{\/+$}, "")
       api_version = opts[:api_version] || VSTS.configuration.api_version
       collection = opts[:collection] || VSTS.configuration.collection
@@ -103,7 +105,7 @@ module VSTS
 
       base = [base_url, collection, team_project, "_apis", area, resource].compact.join("/")
       urlparams["api-version"] ||= api_version
-      url_encoded_params = URI.encode_www_form(urlparams)
+      url_encoded_params = URI.encode_www_form(urlparams) # makes url params from Hash
 
       base + "?" + url_encoded_params
     end
@@ -111,8 +113,38 @@ module VSTS
     # Calculate the Authorization header for the API based on the personal access token
     #
     # @return [String] the Authorization header value for Basic auth, ie. "Basic jrigf9404vvxsoi48t048fdj=="
-    def authz_header_value
+    # @private
+    def self.authz_header_value
       "Basic " + Base64.strict_encode64(":" + VSTS.configuration.personal_access_token)
+    end
+
+    # Build URL parameters hash from options hash (used internally)
+    #
+    # @param opts [Hash] options hash with Symbol keys to build url parameters from
+    # @param paramnames [Array<String>, Array<Symbol>, Array<Array<String, String>>, Array<Hash>]
+    #                   list of VSTS URL parameter names and VSTS prefixes to filter from opts
+    # @return [Hash] url parameters hash
+    # @private
+    def self.build_params(opts, paramnames)
+      urlparams = {}
+      paramnames.each do |paramname_with_prefix|
+        case paramname_with_prefix
+        when String, Symbol
+          prefix = ""
+          paramname = paramname_with_prefix
+        when Array
+          prefix = paramname_with_prefix[0]
+          paramname = paramname_with_prefix[1]
+        when Hash
+          prefix = paramname_with_prefix.first[0]
+          paramname = paramname_with_prefix.first[1]
+        else
+          VSTS.logger.warn("Invalid type in paramlist in APIClient##build_params: #{paramname_with_prefix.class}")
+          next
+        end
+        urlparams["#{prefix}#{paramname}"] = opts[paramname] if opts[paramname]
+      end
+      urlparams
     end
   end
 end
