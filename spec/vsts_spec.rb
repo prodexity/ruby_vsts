7ab50d5bbbff25b1b2b331576201b4f721b1d933
Gabor Lengyel <glengyel@prodexity.com>
2017-04-10 00:58:28 +0200

    added test for version

--- spec/vsts_spec.rb
+++ spec/vsts_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe VSTS do
   describe 'logging' do
+    it 'has a version' do
+      expect(VSTS::VERSION).to match(/\A\d+\.\d+\.\d+(-\w+)?\z/)
+    end
+
     it 'has a logger' do
       expect(described_class.logger).to be_a(Logger)
     end
