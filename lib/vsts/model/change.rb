06cc794f33558f1c9e3da1520332f22503d975f4
Igor da Cunha Souza <igordcsouza@me.com>
2018-05-11 07:57:33 -0300

    giving a real name to initialize parameter

--- lib/vsts/model/change.rb
+++ lib/vsts/model/change.rb
@@ -7,10 +7,10 @@ module VSTS
     # Create new change instance from a hash
     # See https://www.visualstudio.com/en-us/docs/integrate/api/tfvc/changesets#get-list-of-changes-in-a-changeset
     #
-    # @param h [Hash] change data as returned by the VSTS API
-    def initialize(h = {})
-      @change_type = h["changeType"]
-      @item = Item.new(h["item"])
+    # @param changeset [Hash] change data as returned by the VSTS API
+    def initialize(changeset = {})
+      @change_type = changeset["changeType"]
+      @item = Item.new(changeset["item"])
     end
 
     # Convenience method to directly access item version
